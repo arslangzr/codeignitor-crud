327a65d821def6a7279c523ca3aeb9accfc9ad93
Muhammad Arsalan <50544594+arslangzr@users.noreply.github.com>
2023-04-24 02:47:00 +0500

    Defaulted route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,9 +29,10 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
 
 // CRUD RESTful Routes
+$routes->get('/', 'UserCrud::index');
 $routes->get('users-list', 'UserCrud::index');
 $routes->get('user-form', 'UserCrud::create');
 $routes->post('submit-form', 'UserCrud::store');
