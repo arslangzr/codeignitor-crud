36476ba9df8f9329c7f4eac0cebbda0b5adf1743
Muhammad Arsalan <50544594+arslangzr@users.noreply.github.com>
2023-04-24 02:25:47 +0500

    Added CRUD functionality

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,14 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+// CRUD RESTful Routes
+$routes->get('users-list', 'UserCrud::index');
+$routes->get('user-form', 'UserCrud::create');
+$routes->post('submit-form', 'UserCrud::store');
+$routes->get('edit-view/(:num)', 'UserCrud::singleUser/$1');
+$routes->post('update', 'UserCrud::update');
+$routes->get('delete/(:num)', 'UserCrud::delete/$1');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
